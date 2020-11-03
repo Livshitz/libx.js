274c3729f9d920e379a7f8bad17baf3fd2af6ecf
Elya Livshitz <elya.livshitz@gmail.com>
2020-11-03 06:08:13 +0200

    * fix rxjs + add support for browser.platform helper

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,7 +1,9 @@
 import { browserHelpers } from './browserHelpers';
 import req from './require';
+import platform from 'platform';
 
 export const browser = {
     helpers: browserHelpers,
     require: req,
+    platform,
 };
