2b6b20d1b7d7e15bf307e8ea221f838d2ec0172c
Elya Livshitz <elya.livshitz@gmail.com>
2022-01-20 09:01:05 +0200

    * fix bad cross dependency

--- src/extensions/StringExtensions.ts
+++ src/extensions/StringExtensions.ts
@@ -1,5 +1,4 @@
 import capitalize from 'lodash/capitalize';
-import { helpers } from '../helpers';
 import { objectHelpers } from '../helpers/ObjectHelpers';
 
 export class StringExtensions {
@@ -77,19 +76,14 @@ export class StringExtensions {
 
         var obj = arguments[0];
         if (obj !== null && typeof obj === 'object') {
-            const keys = helpers.getMatch(ret, /\{\{(.*?)\}\}/g);
+            const matches = ret?.matchAll(/\{\{(.*?)\}\}/g);
+            const keys = [...matches]?.map((m) => m[1]);
+            // const keys = ret.match(/\{\{(.*?)\}\}/g);
             for (const key of keys) {
                 const val = objectHelpers.getDeep(obj, key, '.');
+                if (val == null) continue;
                 ret = ret.replace('{{' + key + '}}', val);
             }
-            /*
-            var arr = objectHelpers.getCustomProperties(obj);
-            for (var i = 0; i < arr.length; i++) {
-                var x = arr[i];
-                ret = ret.replace('{{' + x + '}}', obj[x]);
-                // ret = ret.replace(new RegExp('\{' + x + '\}', 'g'), obj[x])
-            }
-            */
         } else {
             for (var _i = 0; _i < arguments.length - 0; _i++) {
                 args[_i] = arguments[_i + 0];
