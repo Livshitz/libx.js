a3da36e3c5b5bc264c96baef4b76f18df015c5e9
Elya Livshitz <elya.livshitz@gmail.com>
2023-12-31 11:12:31 +0200

    .

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,10 @@
 import fetch from 'node-fetch'
 globalThis.fetch = fetch
 global.TransformStream = require('web-streams-polyfill').TransformStream;
+import { WritableStream } from 'web-streams-polyfill/ponyfill';
+
+if (!global.WritableStream) {
+	global.WritableStream = <any>WritableStream;
+}
 
 // Run specific test: `yarn jest -t 'string.capitalize' tests/libx.extensions.test.ts`
