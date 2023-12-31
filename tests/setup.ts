8060db6af066e9deeb78d705ee8b098f9b6b13aa
Elya Livshitz <elya.livshitz@gmail.com>
2023-12-31 14:06:53 +0200

    cleanup

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,17 +1,14 @@
-console.log('---------- SETUP!')
+// console.log('---------- SETUP!')
 // import 'isomorphic-fetch';
+/*
 import * as streams from 'web-streams-polyfill';
-import { Response } from 'node-fetch';
-// globalThis.fetch = fetch
+import fetch from 'node-fetch';
 
+globalThis.fetch = fetch
 global.TransformStream = streams.TransformStream;
 global.ReadableStream = streams.ReadableStream;
 global.WritableStream = <any>streams.WritableStream;
-global.Response = Response;
-
-// globalThis.ReadableStream = <any>streams.ReadableStream;
-// globalThis.WritableStream = <any>streams.WritableStream;
-// globalThis.TransformStream = <any>streams.TransformStream;
-
+global.Response = fetch.Response;
+*/
 
 // Run specific test: `yarn jest -t 'string.capitalize' tests/libx.extensions.test.ts`
